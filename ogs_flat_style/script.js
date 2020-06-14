256819392c31ef3a45941d46f6587149efb414b6
aki <michiakig@gmail.com>
2020-06-14 14:30:50 -0400

    initial flat style for white stones

--- ogs_flat_style/script.js
+++ ogs_flat_style/script.js
@@ -1,1 +1,20 @@
-console.log(GoThemes.white.Plain.prototype.getWhiteStoneColor());
+GoThemes.white.Plain.prototype.placeWhiteStone =
+    function(ctx, shadow_ctx, stone, cx, cy, radius) {
+        var color = this.getWhiteStoneColor();
+        let lineWidth = radius * 0.10;
+        if (lineWidth < 0.3) {
+            lineWidth = 0;
+        }
+        ctx.fillStyle = color;
+        ctx.strokeStyle = color;
+        if (lineWidth > 0) {
+            ctx.lineWidth = lineWidth;
+        }
+        ctx.beginPath();
+        ctx.arc(cx, cy, radius - lineWidth / 2, 0.001, 2 * Math.PI, false); /* 0.001 to workaround fucked up chrome bug */
+        if (lineWidth > 0) {
+            ctx.stroke();
+        }
+        ctx.fill();
+    };
+console.log("[ogs flat style] done");
